999fb5bb0d47acea085888b2134600f828931185
Nadia Lee <miss.nadia.lee@gmail.com>
2019-03-08 23:04:42 +0000

    Automatically backed up by Learn

--- src/Matrix.js
+++ src/Matrix.js
@@ -12,7 +12,7 @@ export default class Matrix extends Component {
     }
   }
   
-  setSelectedColor = (newColor) => {
+  setColor = (newColor) => {
     this.setState({
       selectedColor: newColor
     })
