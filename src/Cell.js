d5a34b800ed1ed73399e3f886dbba0dcda035646
Nadia Lee <miss.nadia.lee@gmail.com>
2019-03-08 22:54:22 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Cell extends Component {
   
   constructor(props) {
-    super(props)
+    super()
     this.state = {
       color: this.props.color
     }
